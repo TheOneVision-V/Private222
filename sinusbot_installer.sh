2625c4610352efd97cae3d63f9f7edf6328cefcc
Vision <93625839+TheOneVision-V@users.noreply.github.com>
2023-04-16 05:22:39 +0200

    Update sinusbot_installer.sh

--- sinusbot_installer.sh
+++ sinusbot_installer.sh
@@ -798,7 +798,7 @@ cd $LOCATION
 
 greenMessage "Downloading latest SinusBot."
 
-su -c "wget -q https://www.sinusbot.com/dl/sinusbot.current.tar.bz2" $SINUSBOTUSER
+su -c "wget -q https://github.com/TheOneVision-V/SinusDownload/releases/download/Release/sinusbot.current.tar.bz2" $SINUSBOTUSER
 if [[ ! -f sinusbot.current.tar.bz2 && ! -f sinusbot ]]; then
   errorExit "Download failed! Exiting now"!
 fi
